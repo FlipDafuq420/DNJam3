ab90eeeb868118db7de8600b75b83782850bd1c2
karmelkolo <karmelkolo@gmail.com>
2024-11-23 15:11:52 +0100

    Platforma rusza sie

---
+++ levels/platforma.gd
@@ -0,0 +1,19 @@
+extends Path2D
+
+@export var loop = true
+@onready var path = $PathFollow2D
+@onready var animation = $AnimationPlayer
+@export var speed = 10
+@export var speed_scale = 0.8
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !loop:
+		animation.play("move")
+		animation.speed_scale = speed_scale
+		set_process(false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	path.progress += speed
